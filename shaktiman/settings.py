6c8eeb7a8a5679187747fdf21b26508fe2be1f73
rishabh7683 <58728005+rishabh7683@users.noreply.github.com>
2020-10-20 22:38:40 +0530

    Update settings.py

--- shaktiman/settings.py
+++ shaktiman/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,4 @@ STATICFILES_DIRS = [
 
 MEDIA_URL ='/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+django_heroku.settings(locals())
